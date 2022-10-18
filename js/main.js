abd5f43ec589faf2cb582561e366cddabf49b629
Ahmedtarekelhelw <elhelwahmed4@gmail.com>
2022-10-18 17:03:26 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let li = document.querySelectorAll("#portfolio-filters li");
+let imgContainer = document.querySelectorAll(
+  ".portfolio-container .portfolio-item "
+);
+
+// remove and add class active to click li
+li.forEach((e) =>
+  e.addEventListener("click", (ele) => {
+    //remove all class active from li
+    li.forEach((el) => el.classList.remove("active"));
+
+    // then add class active to clicked li
+    ele.currentTarget.classList.add("active");
+
+    // appear image that accociated with clicked li
+    let dataValue = document.querySelectorAll(ele.currentTarget.dataset.filter);
+    imgContainer.forEach((e) => {
+      e.style.transform = " scale(0)";
+      e.style.visibility = "hidden";
+    });
+
+    dataValue.forEach((e) => {
+      e.style.transform = "scale(1)";
+      e.style.visibility = "visible";
+    });
+  })
+);
+
+// Back to top button
+const btn = document.querySelector(".back-to-top");
+btn.onclick = () => {
+  window.scrollTo({ top: 0 });
+};
